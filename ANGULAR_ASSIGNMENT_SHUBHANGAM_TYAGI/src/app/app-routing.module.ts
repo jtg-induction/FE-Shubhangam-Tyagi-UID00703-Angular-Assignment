192033186a8280fe40b38e44105944af2ee78984
shubhangam tyagi <shubhangam.tyagi@joshtechnologygroup.com>
2026-02-03 17:27:02 +0530

    temp

--- ANGULAR_ASSIGNMENT_SHUBHANGAM_TYAGI/src/app/app-routing.module.ts
+++ ANGULAR_ASSIGNMENT_SHUBHANGAM_TYAGI/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 import { HomePageComponent } from './core/home-page/home-page.component';
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
+  {path: '', component: HomePageComponent },
   { path: '**', component: PageNotFoundComponent }, // Wildcard
 ];
 
