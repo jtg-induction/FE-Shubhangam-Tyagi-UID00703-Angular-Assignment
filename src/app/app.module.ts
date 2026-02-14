62a4c2ce7436bfcdfa6e69ff8a635276d166f9c0
shubhangam tyagi <shubhangam.tyagi@joshtechnologygroup.com>
2026-02-14 14:38:06 +0530

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { SharedModule } from '@shared/shared.module';
 import { AuthInterceptor } from '@core/interceptors/auth-interceptor';
+import { MainLayoutComponent } from '@core/layouts/main-layout/main-layout.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainLayoutComponent],
   imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, SharedModule, AppRoutingModule],
   providers: [provideAnimationsAsync(), { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
   bootstrap: [AppComponent],
