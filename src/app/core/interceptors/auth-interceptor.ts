718e5e77eb7fc5caa958b8985f358b69e3d0eb37
shubhangam tyagi <shubhangam.tyagi@joshtechnologygroup.com>
2026-02-16 12:25:18 +0530

    Handled Edge case for token change in local storage

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -1,13 +1,19 @@
 import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { exhaustMap, Observable, take } from 'rxjs';
+import { catchError, exhaustMap, Observable, take, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 import { AuthService } from '@core/services/auth-service';
+import { TokenService } from '@core/services/token-service';
+import { NotificationService } from '@core/services/notification.service';
 /**
  * Interceptor for intercepting API requests
  */
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   authService: AuthService = inject(AuthService);
+  tokenService: TokenService = inject(TokenService);
+  notificationService: NotificationService = inject(NotificationService);
+  router: Router = inject(Router);
   intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     // this function  called for intercepting req
     return this.authService.userToken.pipe(
@@ -20,6 +26,14 @@ export class AuthInterceptor implements HttpInterceptor {
           headers: new HttpHeaders().set('Authorization', `Bearer ${token}`),
         });
         return next.handle(modifiedReq);
+      }),
+      catchError(error => {
+        if (error.status === 401) {
+          this.tokenService.deleteToken();
+          this.notificationService.showErrorSnackBar(error.error.message);
+          this.router.navigate(['/auth/login']);
+        }
+        return throwError(() => error);
       })
     );
   }
