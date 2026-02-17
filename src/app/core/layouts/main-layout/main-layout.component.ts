8a0958e572338394df65e7bf7ef86a45005e7d41
shubhangam tyagi <shubhangam.tyagi@joshtechnologygroup.com>
2026-02-17 19:55:28 +0530

    Final FIxes Done

--- src/app/core/layouts/main-layout/main-layout.component.ts
+++ src/app/core/layouts/main-layout/main-layout.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { AuthService } from '@core/services/auth.service';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
-export class MainLayoutComponent {}
+export class MainLayoutComponent implements OnInit {
+  authService: AuthService = inject(AuthService);
+  destroyRef = inject(DestroyRef);
+  isLoggedIn = false;
+  ngOnInit(): void {
+    this.authService.userToken.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(token => {
+      this.isLoggedIn = !!token || token !== '';
+    });
+  }
+}
--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomepageComponent } from './homepage/homepage.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ArticleDetailsPageComponent } from './pages/article-details-page/article-details-page.component';
 
-const routes: Routes = [{ path: '', component: HomepageComponent }];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'articles/:id', component: ArticleDetailsPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
