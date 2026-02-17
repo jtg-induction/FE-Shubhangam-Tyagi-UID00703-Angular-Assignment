f5ed166d0709af79dbe6f0c06a374d6512f2f625
shubhangam tyagi <shubhangam.tyagi@joshtechnologygroup.com>
2026-02-17 11:39:33 +0530

    Fixes done

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,10 +1,10 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 /**
  * Interceptor for intercepting API requests
  */
-@Injectable()
+
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     // this function  called for intercepting req
